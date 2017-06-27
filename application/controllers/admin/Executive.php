ca336949c008f3945263f37d2a849f8eaa1ca094
Alamgir <alamgir@traveliciousholiday.in>
2017-06-27 19:07:55 +0530

    set session id in excutive.php Controller inside admin panel

--- application/controllers/admin/Executive.php
+++ application/controllers/admin/Executive.php
@@ -9,11 +9,18 @@ class Executive extends BackendController {
     }
 
     public function index() {
+
+        if(!isset($this->session->userdata['logged_in'])){
+            $this->session->set_flashdata('no_user', 'Please Login');
+                 redirect('admin/welcome');
+        }
+        
         $data['customer_list']=$this->Executive_model->fetch_customer();
         $data['page_title'] = 'View Customer';
         $data['breadcrumb'] = 'View Customer';
         $data['main_content'] = 'admin/executive/view_customer';
         $this->load->view('admin/layouts/home',$data);
+
       
     }
 
